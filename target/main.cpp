c4451e5b38dbd74f0b6ef15af1bfec57321fe11e
Max <max@karl.wf>
2023-11-19 15:22:57 +0000

    Use static variables for configuration

--- target/main.cpp
+++ target/main.cpp
@@ -17,6 +17,12 @@ static const unsigned int PORT = 4711;
 // Max allowed temperature before switching to alarm state (in degree celsius)
 static const float LIMIT_OVERTEMP = 25;
 
+// Temperature sending interval
+static const std::chrono::duration SEND_TEMP_INT = 1s;
+
+// Max number of retries to measure temperature before re-initialize sensor
+static const unsigned int SENSOR_MAX_RETRY = 5;
+
 
 int main(void)
 {
@@ -49,7 +55,7 @@ int main(void)
 
     // Main loop
     bool alarm_present = false;
-    int sensor_error = 0;
+    unsigned int sensor_error = 0;
     while (true) {
         auto time_before = std::chrono::system_clock::now();
 
@@ -59,7 +65,7 @@ int main(void)
 
             // Sleep for the right amount of time (roughly)
             auto time_after = std::chrono::system_clock::now();
-            auto time_diff = 1s - (time_after - time_before);
+            auto time_diff = SEND_TEMP_INT - (time_after - time_before);
             if (time_diff > 0ms) {
                 std::this_thread::sleep_for(time_diff);
             }
@@ -90,7 +96,7 @@ int main(void)
         }
 
         // Take care in case the sensor stopped working
-        if (sensor_error >= 5) {
+        if (sensor_error >= SENSOR_MAX_RETRY) {
             std::cerr << "Restart sensor" << std::endl;
             sensor.close();
             sensor.init();
@@ -100,4 +106,3 @@ int main(void)
 
     return 0;
 }
-
